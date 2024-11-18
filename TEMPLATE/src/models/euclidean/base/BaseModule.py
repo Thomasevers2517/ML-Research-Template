fixed,  causal attention not for VIT haha. Also logging the attention maps with cls token

TEMPLATE/src/models/euclidean/base/BaseModule.py
@@ -32,13 +32,16 @@ class BaseModule(torch.nn.Module):
                 self.activations[name] = output.detach()
             elif isinstance(output, (tuple, list)):
                 # Detach each tensor within the tuple or list
-                detached = []
+                elements = []
                 for o in output:
                     if isinstance(o, torch.Tensor):
-                        detached.append(o.detach())
+                        elements.append(o.detach())
+                    elif o is None:
+                        pass
                     else:
-                        detached.append(o)  # Keep non-tensor elements as is
-                self.activations[name] = detached if isinstance(output, list) else tuple(detached)
+                        raise Warning(f"Unhandled layer output type: {type(o)}")
+                        
+                self.activations[name] = torch.stack(elements)
             else:
                 raise Warning(f"Unhandled output type: {type(output)}")
                 self.activations[name] = output  # Store as is or handle accordingly

TEMPLATE/src/models/euclidean/image/VIT.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 from src.models.euclidean.base.BaseModule import BaseModule
-
+from src.models.euclidean.base.Tranformer.Transformer import Block
 class VIT(BaseModule):
-    def __init__(self, input_shape: tuple, output_shape: tuple, num_layers: int, embedding_size: int, num_heads: int, patch_size: int):
+    def __init__(self, input_shape: tuple, output_shape: tuple, num_layers: int, embedding_size: int, 
+                 num_heads: int, patch_size: int, dropout: float = 0.4):
         super(VIT, self).__init__(input_shape=input_shape, output_shape=output_shape)
+        
         self.patch_size = patch_size
         self.num_patches = (input_shape[1] // patch_size) * (input_shape[2] // patch_size)
         self.embed_dim = embedding_size
@@ -18,8 +20,7 @@ class VIT(BaseModule):
         self.positional_embeddings = nn.Parameter(torch.zeros(1, self.num_patches + 1, self.embed_dim))
 
         # Transformer encoder
-        encoder_layer = nn.TransformerEncoderLayer(d_model=self.embed_dim, nhead=num_heads)
-        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+        self.transformer = nn.Sequential(*[Block( self.num_patches+1, self.embed_dim, num_heads, dropout, dropout) for _ in range(num_layers)])
 
         # MLP head for classification
         self.mlp_cls = nn.Linear(self.embed_dim, self.output_size)

TEMPLATE/src/training/base/BaseTrainer.py
@@ -5,7 +5,8 @@ from torch.nn import Module
 from typing import Callable
 import wandb
 from tqdm import tqdm
-
+import numpy as np
+import re
 
 class BaseTrainer:
     def __init__(self, model: Module, train_loader: DataLoader, val_loader: DataLoader, optimizer: Optimizer, loss_fn: Callable, device: torch.device, 
@@ -33,7 +34,7 @@ class BaseTrainer:
         self.log_interval = log_interval
         
         # Flag to check if it is the first batch of a validation step
-        self.first_batch = False
+        self.log_batch = False
         
     def train(self, epochs: int) -> None:
         """
@@ -117,10 +118,9 @@ class BaseTrainer:
         """
         Callback function that is called at the end of each batch.
         """
-        if self.first_batch:
+        if self.log_batch:
             self.log_hooks()
-            self.first_batch = False    
-            
+            self.log_batch = False    
         pass
     def log_hooks(self) -> None:
         self.log_activations(self.model.activations)
@@ -136,12 +136,29 @@ class BaseTrainer:
         """
         if n_samples_to_log > len(activations[list(activations.keys())[0]]):
             n_samples_to_log = len(activations[list(activations.keys())[0]])
-            raise Warning("num_batches is greater than the number of samples in the batch")
-
-        for name, activation in activations.items():
-            for sample in range(n_samples_to_log):
-                wandb.log({f'activations/{sample}/{name}': wandb.Histogram(activation.cpu().numpy()[sample])})
+            raise Warning("num_samples_to_log is greater than the number of samples in the batch")
+        
+        for sample_idx in range(n_samples_to_log):
+            for name, activation in activations.items():
+                activation = activation.cpu().numpy()
+                if re.match(r"transformer\.\d+\.attn\.att_map", name):
+                    self.log_att_map(name=name, sample_idx=sample_idx , att_map=activation[sample_idx])
+                    
         self.model.activations.clear()
+        
+    def log_att_map(self, name, sample_idx, att_map: np.ndarray) -> None:
+        
+        self.log_cls_attmap(name, sample_idx, att_map)
+                       
+    def log_cls_attmap(self, name, sample_idx, att_map: np.ndarray) -> None:
+        cls_attentions = att_map[:, 1:, 0] *255
+        cls_attentions = np.reshape(cls_attentions, (cls_attentions.shape[0], 
+                                                        np.sqrt(cls_attentions.shape[1]).astype(int), 
+                                                        np.sqrt(cls_attentions.shape[1]).astype(int)))
+        nH = cls_attentions.shape[0] # Number of heads
+        layer_idx = name.split('.')[1]
+        for i in range(nH):
+            wandb.log({f'activations/{sample_idx}/layer{layer_idx}/cls_attention/head{i}': wandb.Image(cls_attentions[i,:,:])})    
         
     def train_batch(self, inputs: torch.Tensor, targets: torch.Tensor, log_level : int = None) -> float:
         """
@@ -175,9 +192,11 @@ class BaseTrainer:
         Returns:
             float: The loss value for the batch.
         """
+        
         inputs, targets = inputs.to(self.device), targets.to(self.device)
         outputs = self.model(inputs)
         loss = self.loss_fn(outputs, targets)
+        
         
         return loss
     
@@ -195,29 +214,33 @@ class BaseTrainer:
         total_loss = 0
         with torch.no_grad():
             for [inputs, targets] in dataloader:
+                self.on_batch_start()
                 loss = self.calculate_batch_loss(inputs, targets)
+                self.on_batch_end()
                 total_loss += loss.item()
         avg_loss = total_loss / len(dataloader)
         
         return avg_loss
     
     def validate(self) -> float:
-        self.on_validation_start()
         """
         Validates the model on the validation dataset.
 
         Returns:
             float: The average loss on the validation dataset.
         """
+        self.on_validation_start()
+        
         val_loss = self.calculate_data_loss(self.val_loader) 
         
         self.on_validation_end(val_loss=val_loss)
         return val_loss
+    
     def on_validation_start(self) -> None:
         """
         Callback function that is called at the start of the validation step.
         """
-        self.first_batch = True
+        self.log_batch = True
         self.model.register_hooks()
     
     def on_validation_end(self, val_loss: float) -> None:

TEMPLATE/test.py
@@ -1,7 +1,7 @@
 import src.data.MNIST_Dataloader as MNIST_Dataloader
 from src.training.base.BaseTrainer import BaseTrainer
 import src.models.euclidean.image.ImageMLP as ImageMLP
-import src.models.euclidean.base.Tranformer.VIT as VIT
+import src.models.euclidean.image.VIT as VIT
 import torch
 import wandb
 
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     # Initialize wandb
     wandb.init(project="template_test", dir="TEMPLATE/log")
 
-    train_loader, val_loader, test_loader = MNIST_Dataloader.get_dataloaders('TEMPLATE/data/MNIST/raw', 1024)
+    train_loader, val_loader, test_loader = MNIST_Dataloader.get_dataloaders('TEMPLATE/data/MNIST/raw', 128)
     
     inputs, targets = next(iter(train_loader))
 
@@ -26,18 +26,18 @@ if __name__ == '__main__':
     print(f"Input shape: {input_shape}, Output shape: {targets_shape}")
    
     
-    model = ImageMLP.ImageMLP(input_shape=input_shape, output_shape=targets_shape, 
-    num_layers=1, hidden_size=50).to(device)
+    # model = ImageMLP.ImageMLP(input_shape=input_shape, output_shape=targets_shape, 
+    # num_layers=1, hidden_size=50).to(device)
     
-    # model = VIT.VIT(input_shape=input_shape, output_shape=targets_shape, 
-    #                 num_layers=1, embedding_size=1, num_heads=1, patch_size=4).to(device)
+    model = VIT.VIT(input_shape=input_shape, output_shape=targets_shape, 
+                    num_layers=2, embedding_size=16, num_heads=2, patch_size=2).to(device)
     wandb.watch(model)
     
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
     loss_fn = torch.nn.CrossEntropyLoss()
     trainer = BaseTrainer(model, train_loader, val_loader, optimizer, loss_fn, device)
     
-    trainer.train(epochs=1)
+    trainer.train(epochs=5)
     wandb.log({"test_loss": trainer.test(test_loader=test_loader)})
     
     