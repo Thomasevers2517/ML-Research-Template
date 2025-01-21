a4b731aa429f36545c4dce0eb3ee21365c70e220
Thomasevers2517 <thomasevers9@gmail.com>
2025-01-21 14:29:04 +0100

    fix treemlpV3, the avg ing was not done correctly. Refactored equalize parents into a avg siblings function

--- TEMPLATE/src/models/euclidean/base/Treensformer/TreeAttentionV2.py
+++ TEMPLATE/src/models/euclidean/base/Treensformer/TreeAttentionV2.py
@@ -27,9 +27,26 @@ class TreeAttentionV2(nn.Module):
 
     def forward(self, x):
         B, N_PATCHES, N_LEVELS, R = x.size()
-        N_NODES = N_PATCHES * N_LEVELS
+        N_NODES = 0
+        for i in range(N_LEVELS):
+            N_NODES += 4**i
         
-        q = torch.zeros(B, N_PATCHES, N_LEVELS, R, self.n_embd//self.n_head).to(x.device)
+        x_nodes = torch.zeros(B, N_NODES, N_LEVELS, R)
+        
+        for i in range(N_LEVELS):
+            h_num_sum = 2**i
+            w_num_sum = 2**i
+            h_n_splits = N_PATCHES//h_num_sum
+            w_n_splits = N_PATCHES//w_num_sum
+            for j in range(h_num_sum):
+                for k in range(w_num_sum):
+                    local_h = int(j*h_n_splits)
+                    local_w = int(k*w_n_splits)
+                    x_temp =  x[:, :, :, i, :].reshape(B, h_n_splits,h_num_sum, w_n_splits, w_num_sum, R)
+                    x_temp =  x_temp.mean(dim=[2, 4]) # B, h_n_splits, w_n_splits, R
+                    
+        
+        q = torch.zeros(B, self.n_head, N_NODES, N_LEVELS * R/self.n_head)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
         q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
--- TEMPLATE/src/models/euclidean/base/Treensformer/TreensformerV4.py
+++ TEMPLATE/src/models/euclidean/base/Treensformer/TreensformerV4.py
@@ -59,8 +59,7 @@ class TreensformerBlockV4(nn.Module):
         x = self.equalize_parents(x)
         x = x.view(B, N_PATCHES, C)
         
-        x = x.view(B, N_PATCHES, N_LEVELS, R)
-        
+        x = x.view(B, H, W, N_LEVELS, R)
         
         x = x + self.tree_mlp(self.ln_2(x))
         
@@ -73,20 +72,37 @@ class TreensformerBlockV4(nn.Module):
         h_summary_size  = 2
         w_summary_size = 2
         for i in range(N_LEVELS):
-            h_num_sum = h_summary_size**i
-            w_num_sum = w_summary_size**i
-            h_n_splits = H//h_num_sum
-            w_n_splits = W//w_num_sum
-            assert isinstance(h_n_splits, int), "h_n_splits must be an integer"
-            assert isinstance(w_n_splits, int), "w_n_splits must be an integer"
-            h_n_splits = int(h_n_splits)
-            w_n_splits = int(w_n_splits)
-            
-            x_temp =  x[:, :, :, i, :].reshape(B, h_n_splits,h_num_sum, w_n_splits, w_num_sum, R)
-            x_temp =  x_temp.mean(dim=[2, 4]) # B, h_n_splits, w_n_splits, R
-            x_temp = torch.repeat_interleave(x_temp, repeats=h_num_sum, dim=1)
-            x[:, :, :, i, :] = torch.repeat_interleave(x_temp, repeats=w_num_sum, dim=2)
+            x[:, :, :, i, :] = avg_siblings(x[:, :, :, i, :], sibling_order=i, h_summary_size=h_summary_size, w_summary_size=w_summary_size)
         return x
+    
+def avg_siblings(self, x,sibling_order, h_summary_size=1, w_summary_size=1):
+    """ Average the siblings of a node in the tree.
+    Args:
+        x: torch.Tensor of shape (B, H, W, R)
+        sibling_order: int, the order of the siblings to average
+        h_summary_size: int, the size of the summary in the height dimension
+        w_summary_size: int, the size of the summary in the width dimension
+        Returns:
+        x: torch.Tensor of shape (B, H, W, R)
+        """
+    
+    B, H, W, R = x.size()
+    
+    h_num_sum = h_summary_size**sibling_order
+    w_num_sum = w_summary_size**sibling_order
+    h_n_splits = H//h_num_sum
+    w_n_splits = W//w_num_sum
+
+    assert isinstance(h_n_splits, int), "h_n_splits must be an integer"
+    assert isinstance(w_n_splits, int), "w_n_splits must be an integer"
+    h_n_splits = int(h_n_splits)
+    w_n_splits = int(w_n_splits)
+    
+    x_temp =  x.reshape(B, h_n_splits,h_num_sum, w_n_splits, w_num_sum, R)
+    x_temp =  x_temp.mean(dim=[2, 4]) # B, h_n_splits, w_n_splits, R
+    x_temp = torch.repeat_interleave(x_temp, repeats=h_num_sum, dim=1)
+    return torch.repeat_interleave(x_temp, repeats=w_num_sum, dim=2)
+
     
 class TreeMLP(nn.Module):
     def __init__(self, n_embd, n_levels):
@@ -151,15 +167,21 @@ class TreeMLPV3(nn.Module):
         )
         
     def forward(self, x):
-        B, N_PATCHES, N_LEVELS, R = x.size()
+        B, H, W, N_LEVELS, R = x.size()
         
         for i in range(N_LEVELS):
             
-            input = x[:, :, i:, :].view(B, N_PATCHES, (N_LEVELS-i)*R)
-            avgd = x[:, :, :i, :].view(B, N_PATCHES, i*R)
-            avgd = avgd.mean(dim=1, keepdim=True)
-            avgd = avgd.repeat(1, N_PATCHES, 1)
+            input = x[:, :, :, i:, :].view(B, H, W, (N_LEVELS-i)*R)
+            if i == 0:
+                x[:, :, :, i, :] = self.mlp(input)
+                continue
+            
+            avgd = x[:, :, :, :i, :].view(B, H, W, i*R)
+            avgd = avgd.view(B, H, W, i, R)
+            for j in range(i):
+                avgd = avg_siblings(avgd[:, :, :, j, :], sibling_order=i, h_summary_size=2, w_summary_size=2)
             input = torch.concatenate([avgd, input], dim=2)
-            x[:, :, i, :] = self.mlp(input)
+            
+            x[:, :, :, i, :] = self.mlp(input)
 
         return x
