testing v3, its like v1 but averaged values instead of zeros are placed. This may fix the overfitting v1 experienced.

TEMPLATE/main.py
@@ -133,7 +133,9 @@ if __name__ == '__main__':
         print("Compiling model")
         model = torch.compile(model, mode=TRAIN_CONFIG['COMPILATION']['MODE'])
         print("Model compiled")
-    wandb.watch(model)
+        
+    #for watching gradients and parameters
+    # wandb.watch(model)
 
     
     optimizer = torch.optim.AdamW(model.parameters(), lr=TRAIN_CONFIG['OPTIMIZER_PARAMS']['LR'])

TEMPLATE/src/models/euclidean/base/Treensformer/TreeAttentionV2.py
@@ -3,12 +3,8 @@ import torch.nn as nn
 import math
 import torch.nn.functional as F
 
-class TreeAttention(nn.Module):
-    """
-    A vanilla multi-head masked self-attention layer with a projection at the end.
-    It is possible to use torch.nn.MultiheadAttention here but I am including an
-    explicit implementation here to show that there is nothing too scary here.
-    """
+class TreeAttentionV2(nn.Module):
+ 
 
     def __init__(self, block_size, n_embd, n_head, attn_pdrop, resid_pdrop, T_Threshold=0):
         
@@ -21,9 +17,7 @@ class TreeAttention(nn.Module):
         # regularization
         self.attn_dropout = nn.Dropout(attn_pdrop)
         self.resid_dropout = nn.Dropout(resid_pdrop)
-        # causal mask to ensure that attention is only applied to the left in the input sequence
-        self.register_buffer("bias", torch.tril(torch.ones(block_size, block_size))
-                                     .view(1, 1, block_size, block_size))
+
         # just so that the attention map is logged
         self.att_map = AttentionMap()
         
@@ -33,10 +27,9 @@ class TreeAttention(nn.Module):
 
     def forward(self, x):
         B, N_PATCHES, N_LEVELS, R = x.size()
+        N_NODES = N_PATCHES * N_LEVELS
         
-        x = x.view(B, N_PATCHES, N_LEVELS*R)
-        T = N_PATCHES
-        C = N_LEVELS*R
+        q = torch.zeros(B, N_PATCHES, N_LEVELS, R, self.n_embd//self.n_head).to(x.device)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
         q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)

TEMPLATE/src/models/euclidean/base/Treensformer/TreensformerV4.py
@@ -36,7 +36,7 @@ class TreensformerBlockV4(nn.Module):
         self.ln_1 = nn.LayerNorm(n_embd//n_levels)
         self.ln_2 = nn.LayerNorm(n_embd//n_levels)
 
-        self.tree_mlp = TreeMLPV2(n_embd, n_levels)
+        self.tree_mlp = TreeMLPV3(n_embd, n_levels)
         
         self.reg_loss = torch.tensor(0.0, requires_grad=True)
 
@@ -136,4 +136,30 @@ class TreeMLPV2(nn.Module):
         
         # Stack outputs along the level dimension
         x = torch.stack(outputs, dim=2)  # Shape: [B, N_PATCHES, N_LEVELS, R]
+        return x
+    
+class TreeMLPV3(nn.Module):
+    def __init__(self, n_embd, n_levels):
+        super().__init__()
+        self.n_embd = n_embd
+        self.n_levels = n_levels
+        
+        self.mlp = nn.Sequential(
+            nn.Linear(n_embd, n_embd * 4),
+            NewGELU(),
+            nn.Linear(n_embd * 4, n_embd//n_levels),
+        )
+        
+    def forward(self, x):
+        B, N_PATCHES, N_LEVELS, R = x.size()
+        
+        for i in range(N_LEVELS):
+            
+            input = x[:, :, i:, :].view(B, N_PATCHES, (N_LEVELS-i)*R)
+            avgd = x[:, :, :i, :].view(B, N_PATCHES, i*R)
+            avgd = avgd.mean(dim=1, keepdim=True)
+            avgd = avgd.repeat(1, N_PATCHES, 1)
+            input = torch.concatenate([avgd, input], dim=2)
+            x[:, :, i, :] = self.mlp(input)
+
         return x